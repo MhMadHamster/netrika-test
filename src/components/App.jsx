30f49e187ef360ced2f51b813904335887f82cbc
MhMadHamster <mhmad.hamster@gmail.com>
2017-03-25 16:55:35 +0300

    App component refactoring

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,36 @@
-import React, { Component } from 'react';
+import React, { PropTypes } from 'react';
 import Button from './Button';
 import Menu from './Menu';
 
-class App extends Component {
-  render() {
-    const { menuVisibility, toggleMenu, arrItems } = this.props;
-    const text = menuVisibility ? 'close' : 'open';
-    return (
-      <div className="app">
-        <div className="mainBlock">
-          <Button
-            onClick={toggleMenu}
-            text={text}
-          />
-          <ul>
-            {arrItems.map(item => (
-              item.active ? item.text : null
-            ))}
-          </ul>
-        </div>
-        { menuVisibility ? <Menu {...this.props} /> : null}
+const App = (props) => {
+  const { menuVisibility, toggleMenu, arrItems } = props;
+  const text = menuVisibility ? 'close' : 'open';
+  return (
+    <div className="app">
+      <div className="mainBlock">
+        <Button
+          onClick={toggleMenu}
+          text={text}
+        />
+        <ul>
+          {arrItems.map(item => (
+            item.active ? item.text : null
+          ))}
+        </ul>
       </div>
-    );
-  }
-}
+      { menuVisibility ? <Menu {...props} /> : null}
+    </div>
+  );
+};
+
+App.propTypes = {
+  menuVisibility: PropTypes.bool.isRequired,
+  toggleMenu: PropTypes.func.isRequired,
+  arrItems: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    active: PropTypes.bool.isRequired,
+    text: PropTypes.string,
+  }).isRequired,
+};
 
 export default App;
